7fea109b6e5b329f747e7d44777382590d1f0521
Denis Kelolli <deniskelolli@gmail.com>
2023-05-28 12:51:06 -0400

    5 round game

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function getComputerChoice (array){
 
 
 
-function playGame(playerSelection,computerSelection){
+function playRound(playerSelection,computerSelection){
     if ((playerSelection === "rock" && computerSelection === "rock") ||
        ((playerSelection === "paper" && computerSelection === "paper")) ||
        (playerSelection === "scissors" && computerSelection === "scissors")){
@@ -27,13 +27,41 @@ function playGame(playerSelection,computerSelection){
     else if ((playerSelection === "rock" && computerSelection === "scissors") ||
             ((playerSelection === "paper" && computerSelection === "rock")) ||
             (playerSelection === "scissors" && computerSelection === "paper")){
-                return "The player is the winner";
+                return "The player is the winner of this round";
     }
     else if ((playerSelection === "scissors" && computerSelection === "rock") ||
             ((playerSelection === "rock" && computerSelection === "paper")) ||
             (playerSelection === "paper" && computerSelection === "scissors")){
-                return "The computer is the winner";
+                return "The computer is the winner of this round";
             }
 }
 
-console.log(playGame(getPlayerChoice(),getComputerChoice(choices)));
+let playerCount = 0;
+let computerCount = 0;
+
+function game(){
+    while (playerCount < 6 || computerCount < 6){
+       let roundWinner = playRound(getPlayerChoice(),getComputerChoice(choices));
+       if (roundWinner === "There is a tie!") {
+        console.log("There is a tie, no point is given.");
+        
+       } else if (roundWinner === "The player is the winner of this round"){
+        playerCount++;
+        console.log("The player won and their score is: " + playerCount);
+            if (playerCount === 5){
+                console.log("The player has won the game with 5 points!");
+                break;
+            }
+       }
+       else if (roundWinner === "The computer is the winner of this round"){
+        computerCount++;
+        console.log("The computer won and their score is: " + computerCount);
+            if (computerCount === 5){
+                console.log("The computer has won the game with 5 points!");
+                break;
+        }
+       }
+    }       
+}
+
+game();
