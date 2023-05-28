b839867fe104ffe3f970dc2098720df81f752ae2
Denis Kelolli <deniskelolli@gmail.com>
2023-05-28 11:50:52 -0400

    Added logic to choose a winner for a single round

--- script.js
+++ script.js
@@ -2,15 +2,38 @@
 
 const choices = ["rock", "paper", "scissors"];
 
+
+function getPlayerChoice(){
+    playerChoice = prompt("Choose Rock, Paper or Scissors: ").toLowerCase();
+    console.log("The player choice is " + playerChoice);
+    return playerChoice;
+}
+
 function getComputerChoice (array){
     const choicesIndex = Math.floor(Math.random()*array.length);
+    console.log("The computer choice is " + array[choicesIndex]);
     return array[choicesIndex];
 }
 
-function getPlayerChoice(){
-    playerChoice = prompt("Choose Rock, Paper or Scissors: ").toLocaleLowerCase();
-    return "The player chooses: " + playerChoice;
+
+
+
+function playGame(playerSelection,computerSelection){
+    if ((playerSelection === "rock" && computerSelection === "rock") ||
+       ((playerSelection === "paper" && computerSelection === "paper")) ||
+       (playerSelection === "scissors" && computerSelection === "scissors")){
+            return "There is a tie!";
+    }
+    else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+            ((playerSelection === "paper" && computerSelection === "rock")) ||
+            (playerSelection === "scissors" && computerSelection === "paper")){
+                return "The player is the winner";
+    }
+    else if ((playerSelection === "scissors" && computerSelection === "rock") ||
+            ((playerSelection === "rock" && computerSelection === "paper")) ||
+            (playerSelection === "paper" && computerSelection === "scissors")){
+                return "The computer is the winner";
+            }
 }
 
-console.log(getComputerChoice(choices));
-console.log(getPlayerChoice());
+console.log(playGame(getPlayerChoice(),getComputerChoice(choices)));
